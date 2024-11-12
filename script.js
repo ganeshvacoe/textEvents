2eb6a170e809a7ad7106bd71d6e6c062ce4c9a67
ganeshvacoe <ganesh.vacoe@gmail.com>
2024-11-12 22:16:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+  function keyFunction()
+    {
+        console.log("You pressed a key ");
+      }
+  
+      function keyUpFunction(){
+        console.log("You released a key ");
+
+      }
+
+      function mouseDoubleClickFunction(){
+        document.getElementById("mouseDemo").innerHTML += "Double Clicked";
+      }
